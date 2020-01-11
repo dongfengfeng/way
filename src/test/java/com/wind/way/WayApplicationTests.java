a79c91a0b83906e4e1c36e5d9f446f02a8662dff
dongff <745898036@qq.com>
2020-01-11 16:40:09 +0800

    Add files via upload
    init

---
+++ src/test/java/com/wind/way/WayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wind.way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
